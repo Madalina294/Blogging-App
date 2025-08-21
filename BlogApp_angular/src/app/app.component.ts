3c75821bec102f68d9112337222a59a263ae7b8f
Madalina294 <puicanmadalinamaria@gmail.com>
2025-08-21 18:36:51 +0300

    Adde navbar

--- BlogApp_angular/src/app/app.component.ts
+++ BlogApp_angular/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
